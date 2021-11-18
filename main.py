5d91453ff605de05e8fc63d6b6f1ef50c60a3d81
nabil <merchant.nabil@gmail.com>
2021-11-17 20:09:37 -0600

    Adding more examples

--- main.py
+++ main.py
@@ -3,12 +3,95 @@ import tkinter
 window = tkinter.Tk()
 window.title("My first GUI program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # Labels
 my_label = tkinter.Label(text="This is a label", font=("Arial", 24, "italic"))
 my_label.pack(side="left")
 # ^ this is required for label to show up
 
+# my_label["text"] = "Some new text"
+my_label.config(text="Some new text")
+
+
+# Button
+def button_clicked():
+    new_text = input.get()
+    my_label.config(text=new_text)
+    print("I got clicked")
+
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+# button.pack()
+# an alternative to pack is place
+button.place(x=0, y=0)
+
+# new_button = tkinter.Button(text="New Button")
+# new_button.grid(column=2, row=0)
+
+# Entry
+input = tkinter.Entry(width=10)
+input.pack()
+
+
+# Spinbox
+def spinbox_used():
+    # gets the current value in spinbox.
+    print(spinbox.get())
+
+
+spinbox = tkinter.Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+
+# Scale
+# Called with current scale value.
+def scale_used(value):
+    print(value)
+
+
+scale = tkinter.Scale(from_=0, to=100, command=scale_used)
+scale.pack()
+
+
+# Checkbutton
+def checkbutton_used():
+    # Prints 1 if On button checked, otherwise 0.
+    print(checked_state.get())
+
+
+# variable to hold on to checked state, 0 is off, 1 is on.
+checked_state = tkinter.IntVar()
+checkbutton = tkinter.Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
+
+
+# Radiobutton
+def radio_used():
+    print(radio_state.get())
+
+
+# Variable to hold on to which radio button value is checked.
+radio_state = tkinter.IntVar()
+radiobutton1 = tkinter.Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = tkinter.Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+# Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+
+listbox = tkinter.Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
 
 # Required to keep the window open until manually closed by the user.
 window.mainloop()
--- playground.py
+++ playground.py
@@ -9,3 +9,37 @@ def add(*args):
 
 
 print(add(3, 4, 5, 15, 32, 1))
+
+
+def calculate(n, **kwargs):
+    # type of kwargs is dict
+    print(kwargs)
+
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+
+    # print(kwargs["add"])
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kwargs):
+        # self.make = kwargs["make"]
+        self.make = kwargs.get("make")
+        # self.color = kwargs["color"]
+        self.color = kwargs.get("color")
+        self.model = kwargs.get("model")
+
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)
+
+my_car_2 = Car(make="Nissan")
+print(my_car_2.model)
